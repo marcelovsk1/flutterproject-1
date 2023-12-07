7ad51057e28a5b52e1d4a3996b3cbc594e50ef28
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-07 11:07:52 -0300

    working on

---
+++ my_app/lib/Screen/screen_excercise.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart'
+
+class screenExcercise extends StatelessWidget {
+  const screenExcercise({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
