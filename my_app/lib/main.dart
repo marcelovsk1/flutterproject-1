61ebf63075bccb9ca82701c90a05542e5c7ad656
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-08 15:29:13 -0300

    working on

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/Screen/home.dart';
 import 'package:my_app/Screen/screen_excercise.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: screenExcercise(),
+      home: HomeScreen(),
     );
   }
 }
